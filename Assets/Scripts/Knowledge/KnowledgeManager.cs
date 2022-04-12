2c0770eb881a4124b8e5ee7358ea7fd11b2731f9
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-04-12 11:43:41 -0400

    Moved files around

--- Assets/Scripts/Knowledge/KnowledgeManager.cs
+++ Assets/Scripts/Knowledge/KnowledgeManager.cs
@@ -1,4 +1,12 @@
 
+using System.Collections;
+
 public class KnowledgeManager : BaseManager
 {
+    private Knowledge _knowledge;
+
+    public override IEnumerator Initialize()
+    {
+        yield break;
+    }
 }
