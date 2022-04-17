2de2ce9c5d7bb0a47176c2bb0a2a7ea726a88a6e
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-04-17 18:38:18 -0400

    Migrated scoreboard to new render format.

--- Assets/Scripts/Scoreboard/ScoreboardManager.cs
+++ Assets/Scripts/Scoreboard/ScoreboardManager.cs
@@ -1,31 +1,27 @@
 using System.Collections.Generic;
-using UnityEngine;
 
-public class ScoreboardManager : MonoBehaviour,
+public class ScoreboardManager : RendererManager<List<Word>>,
     IUpdateObserver<GuessAnnotated>,
     IUpdateObserver<KnowledgeInitialized> {
-    [SerializeField] private ScoreboardRenderer Renderer;
 
     private List<Word> _correct;
+    private List<Word> Correct => _correct ??= new List<Word>();
     private int AnswersCount {
         set {
-            _correct = new List<Word>();
+            Correct.Clear();
             while (value-- > 0)
-                _correct.Add("");
-            Renderer.Render(_correct);
+                Correct.Add("");
         }
     }
-
+    protected override List<Word> Data => _correct;
     public void Handle(GuessAnnotated update) {
         if (update.AnswerIndex == -1)
             return;
         _correct[update.AnswerIndex] = update.AnnotatedGuess.Word;
-        Renderer.Render(_correct);
     }
 
     public void HandleCorrectGuess(AnnotatedWord correctWord, int index) {
         _correct[index] = correctWord.Word;
-        Renderer.Render(_correct);
     }
 
     public void Handle(KnowledgeInitialized update) {
--- Assets/Scripts/Scoreboard/ScoreboardRenderer.cs
+++ Assets/Scripts/Scoreboard/ScoreboardRenderer.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class ScoreboardRenderer : MonoBehaviour
-{
+public class ScoreboardRenderer : BaseRenderer<List<Word>> {
     [SerializeField] private TextMeshProUGUI text;
-    public void Render(List<Word> correct)
-    {
+    public override void Render(List<Word> correct) {
         text.text = "";
-        for (var i = 0; i < correct.Count; i++)
-        {
-            text.text += string.Format("{0}. {1}\n", i+1,(string)correct[i]);
+        for (var i = 0; i < correct.Count; i++) {
+            text.text += string.Format("{0}. {1}\n", i + 1, (string)correct[i]);
         }
     }
 }
