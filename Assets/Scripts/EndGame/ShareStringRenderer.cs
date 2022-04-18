e6405e8f091a5861ed62a1bdc3f3423147bc4905
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-04-17 23:26:15 -0400

    Removed some unused imports and reformated file.

--- Assets/Scripts/EndGame/ShareStringRenderer.cs
+++ Assets/Scripts/EndGame/ShareStringRenderer.cs
@@ -5,8 +5,7 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShareStringRenderer : MonoBehaviour
-{
+public class ShareStringRenderer : MonoBehaviour {
     [SerializeField] private Button Share;
     [SerializeField] private string Green;
     [SerializeField] private string Yellow;
@@ -17,37 +16,32 @@ public class ShareStringRenderer : MonoBehaviour
 
     private string _renderedGuesses;
 
-    public void Awake()
-    {
+    public void Awake() {
         Share.onClick.AddListener(CopyGuessesToClipboard);
     }
 
     [DllImport("__Internal")]
     private static extern void CopyToClipboard(string text);
 
-    public static void SetText(string text)
-    {
+    public static void SetText(string text) {
         #if UNITY_WEBGL && UNITY_EDITOR == false
             CopyToClipboard(text);
         #else
             GUIUtility.systemCopyBuffer = text;
         #endif
     }
-    private void CopyGuessesToClipboard()
-    {
+    private void CopyGuessesToClipboard() {
         SetText(_renderedGuesses);
         CopyPrompt.PromptAnimation(0.2f, 0.3f, 1.5f);
     }
-    public void RenderGuesses(TimeSpan time, List<AnnotatedWord> guesses, List<Tuple<Word, int>> guessesRequired)
-    {
+    public void RenderGuesses(TimeSpan time, List<AnnotatedWord> guesses, List<Tuple<Word, int>> guessesRequired) {
         var sb = new StringBuilder();
         sb.AppendLine(string.Format("Wordle Blitz - {0}:{1:00.}", time.Minutes, time.Seconds));
 
         var strings = new List<string>();
         var wordToIndex = new Dictionary<Word, int>();
         var guessed = new List<bool>();
-        for (var i = 0; i < guessesRequired.Count; i++)
-        {
+        for (var i = 0; i < guessesRequired.Count; i++) {
             strings.Add("");
             wordToIndex[guessesRequired[i].Item1] = i;
             guessed.Add(false);
@@ -56,28 +50,22 @@ public class ShareStringRenderer : MonoBehaviour
         var currentIndex = 0;
 
 
-        foreach (var word in guesses)
-        {
+        foreach (var word in guesses) {
             while (guessed[currentIndex])
                 currentIndex++;
             if (word.Word == "BLITZ")
                 continue;
 
-            if (word.NoMatching)
-            {
+            if (word.NoMatching) {
                 strings[currentIndex] += Gray;
-            }
-            else if (!word.Correct)
-            {
+            } else if (!word.Correct) {
                 strings[currentIndex] += Yellow;
             }
-            if (wordToIndex.ContainsKey(word.Word))
-            {
+            if (wordToIndex.ContainsKey(word.Word)) {
                 guessed[wordToIndex[word.Word]] = true;
             }
         }
-        foreach (var s in strings)
-        {
+        foreach (var s in strings) {
             sb.Append(s);
             sb.AppendLine(Green);
         }
@@ -87,8 +75,7 @@ public class ShareStringRenderer : MonoBehaviour
         _renderedGuesses = sb.ToString();
     }
 
-    public void Clear()
-    {
+    public void Clear() {
         _renderedGuesses = "";
     }
 }
