3a868069c6e9435e3cba0b2dbb1c5e4873b4c2e5
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-04-18 14:58:43 -0400

    Renamed managers for clarity.

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -1,69 +1,23 @@
-using System.Collections.Generic;
-using UnityEngine;
-using static CommandKeyRenderer;
+﻿using UnityEngine;
 
-public class InputManager : MonoBehaviour,
-    IUpdateObserver<KnowledgeInitialized>,
-    IUpdateObserver<GuessAnnotated>,
-    IUpdateObserver<NewAnswer> {
-    [SerializeField] private KeyboardRenderer Keyboard;
-
-    private Dictionary<char, KeyRenderer> _keys;
-    private Dictionary<char, KeyRenderer> Keys => _keys ??= new Dictionary<char, KeyRenderer>();
-
-    private KnowledgeManager _knowledge;
-    private void InitalizeKeyRenderer(KeyRenderer key, char c) {
-        Keys.Add(c, key);
-        key.Key = c;
-        key.Callback = () => HitKey(c);
-    }
-
-    private void InitalizeCommandKeyRenderer(CommandKeyRenderer key, Command c) {
-        key.C = c;
-        switch (c) {
-            case Command.None:
-                break;
-            case Command.Delete:
-                key.Callback = () => Delete();
-                break;
-            case Command.Enter:
-                key.Callback = () => Enter();
-                break;
+public class NewInputManager : MonoBehaviour {
+    private void OnGUI() {
+        var e = Event.current;
+        if (e == null)
+            return;
+        if (e.type != EventType.KeyDown)
+            return;
+        if (Language.IsAlpha((char)e.keyCode)) {
+            PlayerInput.HitKey((char)e.keyCode).Emit(gameObject);
+            return;
         }
-    }
-
-    private void Awake() {
-        foreach (char c in Language.Alphabet)
-            InitalizeKeyRenderer(Keyboard.AddKey(c), c);
-        InitalizeCommandKeyRenderer(Keyboard.AddCommand(Command.Delete), Command.Delete);
-        InitalizeCommandKeyRenderer(Keyboard.AddCommand(Command.Enter), Command.Enter);
-    }
-
-    private void HitKey(char c) {
-        PlayerInput.HitKey(c).Emit(gameObject);
-    }
-    private void Delete() {
-        PlayerInput.Delete().Emit(gameObject);
-    }
-    private void Enter() {
-        PlayerInput.Enter().Emit(gameObject);
-    }
-
-    public void Handle(KnowledgeInitialized update) {
-        _knowledge = update.Knowledge;
-    }
-
-    public void Handle(GuessAnnotated update) {
-        foreach (var c in Language.Alphabet) {
-            var k = _knowledge.GlobalKnowledge(c);
-            Keys[c].Knowledge = k;
+        if (e.keyCode == KeyCode.Delete || e.keyCode == KeyCode.Backspace) {
+            PlayerInput.Delete().Emit(gameObject);
+            return;
         }
-    }
-
-    public void Handle(NewAnswer update) {
-        foreach (var c in Language.Alphabet) {
-            var k = _knowledge.GlobalKnowledge(c);
-            Keys[c].Knowledge = k;
+        if (e.keyCode == KeyCode.KeypadEnter || e.keyCode == KeyCode.Return) {
+            PlayerInput.Enter().Emit(gameObject);
+            return;
         }
     }
 }
---
+++ Assets/Scripts/Input/KeyboardManager.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using UnityEngine;
+using static CommandKeyRenderer;
+
+public class InputManager : MonoBehaviour,
+    IUpdateObserver<KnowledgeInitialized>,
+    IUpdateObserver<GuessAnnotated>,
+    IUpdateObserver<NewAnswer> {
+    [SerializeField] private KeyboardRenderer Keyboard;
+
+    private Dictionary<char, KeyRenderer> _keys;
+    private Dictionary<char, KeyRenderer> Keys => _keys ??= new Dictionary<char, KeyRenderer>();
+
+    private KnowledgeManager _knowledge;
+    private void InitalizeKeyRenderer(KeyRenderer key, char c) {
+        Keys.Add(c, key);
+        key.Key = c;
+        key.Callback = () => HitKey(c);
+    }
+
+    private void InitalizeCommandKeyRenderer(CommandKeyRenderer key, Command c) {
+        key.C = c;
+        switch (c) {
+            case Command.None:
+                break;
+            case Command.Delete:
+                key.Callback = () => Delete();
+                break;
+            case Command.Enter:
+                key.Callback = () => Enter();
+                break;
+        }
+    }
+
+    private void Awake() {
+        foreach (char c in Language.Alphabet)
+            InitalizeKeyRenderer(Keyboard.AddKey(c), c);
+        InitalizeCommandKeyRenderer(Keyboard.AddCommand(Command.Delete), Command.Delete);
+        InitalizeCommandKeyRenderer(Keyboard.AddCommand(Command.Enter), Command.Enter);
+    }
+
+    private void HitKey(char c) {
+        PlayerInput.HitKey(c).Emit(gameObject);
+    }
+    private void Delete() {
+        PlayerInput.Delete().Emit(gameObject);
+    }
+    private void Enter() {
+        PlayerInput.Enter().Emit(gameObject);
+    }
+
+    public void Handle(KnowledgeInitialized update) {
+        _knowledge = update.Knowledge;
+    }
+
+    public void Handle(GuessAnnotated update) {
+        foreach (var c in Language.Alphabet) {
+            var k = _knowledge.GlobalKnowledge(c);
+            Keys[c].Knowledge = k;
+        }
+    }
+
+    public void Handle(NewAnswer update) {
+        foreach (var c in Language.Alphabet) {
+            var k = _knowledge.GlobalKnowledge(c);
+            Keys[c].Knowledge = k;
+        }
+    }
+}
